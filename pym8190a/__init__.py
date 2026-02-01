d8018b9bbc88863092e81a93fc7995248e108535
Sebastian Zaiser <stanzky@gmail.com>
2026-02-01 14:01:52 +0100

    fix: hardware independent imports for tests

--- pym8190a/__init__.py
+++ pym8190a/__init__.py
@@ -1,9 +1,17 @@
 import numpy as np
 np.set_printoptions(linewidth=500, suppress=True)
 
-from .pym8190a import *
-from . import hardware
-from . import hardware as hw
+# Import submodules that don't require hardware
 from . import elements
 from . import elements as el
 from . import util
+from . import settings
+
+# Import hardware-dependent modules only if dependencies are available
+try:
+    from . import hardware
+    from . import hardware as hw
+    from .pym8190a import *
+except (ImportError, ModuleNotFoundError):
+    # Allow package to be imported without hardware dependencies for testing
+    pass
